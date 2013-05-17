236b394f85b95567fa5c82ff5bb9388277f7be04
jlorenzen <jameslorenzen@gmail.com>
2013-05-17 00:02:18 -0500

    Updated the main Application class to have a getItems() method that returns a simple map of items that can be displayed in the index.jelly file. Also added a action example method doHello that can be called when browsing: http://localhost:8080/hello. Now the Application home page lists 2 URLs and you can click on the link and that will take you to the Item index.jelly page.

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -1,7 +1,32 @@
 package test;
 
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Kohsuke Kawaguchi
  */
 public class Application {
+
+    public Map<String, Item> getItems() {
+        Map<String, Item> items = new HashMap();
+        items.put("1", new Item("item1"));
+        items.put("2", new Item("item2"));
+
+        return items;
+    }
+
+    /**
+     * Action example called when browsing: http://localhost:8080/hello
+     */
+    public void doHello(StaplerRequest request, StaplerResponse response) throws ServletException, IOException {
+        System.out.println("doHello called");
+
+        response.forwardToPreviousPage(request);
+    }
 }
---
+++ src/main/java/test/Item.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Item {
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
